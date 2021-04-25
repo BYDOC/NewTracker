a0a6f1889c41c4b121a14535afd6d41eda6ad7e0
BYDOC <40527236+BYDOC@users.noreply.github.com>
2021-04-25 20:38:44 +0300

    Add project files.

---
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.Models;
+
+
+namespace TrackerLibrary.DataAccess
+
+{
+    public class SqlConnector : IDataConnection
+    {
+        //TODO- Make the CreatePrize actually save to the DB
+        /// <summary>
+        /// saves a new prize to DB
+        /// </summary>
+        /// <param name="model">The prize info</param>
+        /// <returns></returns>
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments"))) 
+            {
+                //using bittiginde connection kapanır
+
+                var p = new DynamicParameters();
+                p.Add("@PlaceNumber", model.PlaceNumber);
+                p.Add("@PlaceName",model.PlaceName);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
+                p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+                
+                model.Id = p.Get<int>("@id");
+
+                return model; 
+            }
+        }
+    }
+}
---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.DataAccess;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        //public static can not be instantiated, everyone can access content
+        public static IDataConnection Connection { get; private set; } 
+        //private koyarak sadece classın icinde degeri değiştirilebilir yaptık
+        //list olarak oluşturduk çünkü hem sqle hem texte kaydedebiliriz
+
+        /* 
+         Connections=new List<IDataConnection>();
+         */
+
+
+        public static void InitializeConnections(DatabaseType db)
+        {
+            if (db == DatabaseType.Sql)
+            {
+                SqlConnector sql = new SqlConnector();
+                Connection=sql;
+                //TODO Create SQL connection
+              
+            }
+            else if (db ==DatabaseType.TextFile)
+            {
+                TextConnector text = new TextConnector();
+                Connection=text;
+                //TODO Create Text Connection
+            }
+        }
+
+        public static string CnnString(string name)
+        {
+            //references altına assembly kısmına system.configuratiın eklenmeli
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
